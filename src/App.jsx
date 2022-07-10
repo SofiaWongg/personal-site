aaf71fc55a4fbe6237153334e6b13a4fdaec3b4e
Sofia Wong <ssofia@ad.unc.edu>
2022-07-10 13:26:57 -0400

    initial styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import Home from './pages/Home/Home.jsx'
+import Nav from './components/Nav/Nav.jsx'
+import About from './pages/About/About'
+import Experience from './pages/Experience/Experience.jsx'
+import Contact from './pages/Contact/Contact'
+import Footer from './components/Footer/Footer'
 
 const App = () => {
   return (
-    <div>Apppp</div>
+    <>
+      <Home />
+      <Nav />
+      <About />
+      <Experience/>
+      <Contact />
+      <Footer />
+    </>
   )
 }
 
