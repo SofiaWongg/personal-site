b7a120431444c53029602ecebd970708d61fc310
Sofia Wong <ssofia@ad.unc.edu>
2022-07-12 15:19:01 -0400

    contact done - one bug- sibmit behind nav

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsLinkedin} from 'react-icons/bs'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com'
 
 const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    
+
+    emailjs.sendForm('service_qa9szgj', 'template_j46300x', form.current, 'NmxHzDJ2RgDn17YQT')
+      .then((form) => {
+        e.target.reset();
+      })
+
+      
+  };
+
+
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get in Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className='container contact__container'>
+        <div className='contact__options'>
+          <article className='contact__option'>
+            <MdOutlineEmail className='contact__option-icon'/>
+            <h4>Email</h4>
+            <h5>sofia.el.wong@gmail.com</h5>
+            <a href='malito:sofia.el.wong@gmail.com' target="_blank">Send a Message</a>
+          </article>
+          <article className='contact__option'>
+            <BsLinkedin className='contact__option-icon'/>
+            <h4>LinkedIn</h4>
+            <a href='https://www.linkedin.com/in/sofiawong2024/' target="_blank">Send a Message</a>
+          </article>
+        </div>
+        <form ref={form} onSubmit={sendEmail}>
+          <input type="text" name='name' placeholder='Your Full name' required/>
+          <input type="email" name='email' placeholder='Your Email' required />
+          <textarea name="message" rows="7" placeholder='Your Message' required></textarea>
+          <button type='submit' className='btn btn-primary'>Send Message</button>
+        </form> 
+      </div>
+    </section>
   )
 }
 
--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -1,9 +1,63 @@
 import React from 'react'
 import './projects.css'
+import IMG1 from '../../assets/assets/portfolio1.jpg'
+import IMG2 from '../../assets/assets/portfolio2.jpg'
+import IMG3 from '../../assets/assets/portfolio3.jpg'
 
 const Projects = () => {
+
+  const data = [
+    {
+      id: 1,
+      image: IMG1,
+      title: 'Title of Project',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    }, 
+
+    {
+      id: 2,
+      image: IMG2,
+      title: 'Title of Project',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    }, 
+
+    {
+      id: 3,
+      image: IMG3,
+      title: 'Title of Project',
+      github: 'https://github.com',
+      demo: 'https://github.com'
+    }
+
+  ]
   return (
-    <section id='projects'>Projects</section>
+    <section id='projects'>
+      <h5>Completed Projects</h5>
+      <h2>Portfolio</h2>
+
+      <div className='container portfolio__container'>
+        {
+          data.map(({id, image, title, github, demo}) => {
+            return(
+                <article key={id} className='portfolio__item'>
+                <div className='portfolio__item-image'>
+                  <img src={image} alt={title} />
+                </div>
+                <h3>{title}</h3>
+                <div className='portfolio__item-cta'>
+                  <a href={github} className='btn' target='_blank'>Github</a>
+                  <a href={demo} className='btn btn-primary' target='_blank'>Live Demo</a>
+                </div>
+            </article>
+            )
+          })
+        }
+
+       
+       </div>
+    </section>
   )
 }
 
