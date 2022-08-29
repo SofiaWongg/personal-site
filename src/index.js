55aa790b3b0d6f4efd6791917dc0a6c0eb50c877
Sofia Wong <ssofia@ad.unc.edu>
2022-08-29 17:01:39 -0400

    updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css'
+import React from 'react';
 
-ReactDOM.render(<App/>, document.querySelector("#root"))
+
+///ReactDOM.render(<App/>, document.querySelector("#root"))
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
